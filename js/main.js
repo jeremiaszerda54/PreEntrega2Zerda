364aa5c79260e95ab7023662262d205e02953a1f
jeremias <jeremiaszr54@gmail.com>
2023-07-04 09:52:46 -0300

    terminando

--- js/main.js
+++ js/main.js
@@ -28,37 +28,6 @@ while(c<=n)
     let apellido=prompt("Ingrese el apellido")
     let DNI=prompt("Ingrese el documento")
     let doc=prompt("Ingrese a que doctor se quiere diriguir \n 1- Terapeuta 2-Doctor \n 3- Psicologo 4-Doctor2")
-    switch (doc)
-    {
-        case 1
-            :
-            doc1=doc1+1;
-            break
-            ;
-            case 2
-            :
-            doc2=doc2+1;
-            break
-            ;
-            case 3
-            :
-            doc3=doc3+1;
-            break
-            ;
-            case 4
-            :
-            doc4=doc4+1;
-            break
-            ;
-        default
-            :
-            do
-            {
-                alert("Debe de ingresar uno de los 4 doctores")
-                doc=prompt("Ingrese a que doctor se quiere diriguir \n 1- Terapeuta 2-Doctor \n 3- Psicologo 4-Doctor2")
-            }while(doc<1 && doc>4);
-            ;
-    }
     turnos.push(new turno(fecha,nombre,apellido,DNI,doc))
     c++;
 }
@@ -67,14 +36,70 @@ for(const tur of turnos)
     console.log(turnos);
 }
 alert("terminado el cargado de los datos");
-alert("Se decia ingresar algunas de estas cosas como: \n 1-encontrar el doctor con mas pacientes \n 2-eliminar un turno \n 3-encontrar el turno de un paciente por su DNI \n 4-Ordenar alfabeticamente los turnos")
+alert("Se decia ingresar algunas de estas cosas como: \n 1-eliminar un turno \n 2-encontrar el turno de un paciente por su apellido \n 3-Ordenar los turnos por :\n edad - alfabeticamente")
 let algomas=Number(prompt("Ingrese la opcion deseado (no, para cancelar alguna opcion"))
 if( algomas!="no"){
     switch(algomas)
-    {
-        case 1:
-        
-        break    
-        ;
+    { 
+        case 1
+            :
+            console.log("Elimar turno")
+            let nombreBuscado = prompt("Ingrese el nombre que desea buscar:");
+            let indicesAEliminar = [];
+            for (let i = 0; i < turnos.length; i++) {
+            if (turnos[i].nom === nombreBuscado) {
+                indicesAEliminar.push(i);
+            }
+            }
+            for (let i = indicesAEliminar.length - 1; i >= 0; i--) {
+            turnos.splice(indicesAEliminar[i], 1);
+            }
+            console.log("Turnos restantes:");
+            for (const tur of turnos) {
+            console.log(tur);
+            }
+            break;
+            ;
+        case 2
+            :
+            console.log("El turno con el apellido es:")
+            let enco=prompt("Ingrese el apellido que decia encontrar y mostraremos su turno")
+            const resultado=turnos.find((el)=>el.ape===enco)
+            console.log(resultado)
+            break
+            ;
+        case 3
+            :
+            let orde=prompt("Ingrese: \n 1 si quiere ordenar por edad de mayor a menos \n - 2 si quiere ordenar alfabeticamente")
+            if(orde=1)
+            {
+                console.log("los turnos seran ordenas por edad desde la mas chica a la mas grande")
+                turnos.sort((a,b)=>a-b);
+                console.log(turnos)
+            }
+            else
+            {
+                if(orde=2)
+                {
+                    console.log("Ordenar los turnos alfabeticamente")
+                    turnos.sort((a,b)=>{
+                        if (a.nom > b.nom){
+                            return 1;
+                        }
+                        if (a.nom < b.nom){
+                            return -1;
+                        }
+                        return 0;
+                        })
+                    console.log(turnos)
+                }
+                else
+                {
+                    alert("no se ingreso nada")
+                }
+            }
+            break
+            ;
+            
     }
 }
